c2283cd4eb592ba18e29277d25f0ace2ce9a45f9
Gitau Gakwa <50299997+gitaugakwa@users.noreply.github.com>
2025-01-21 11:14:07 +0300

    Update cloudfront.go

--- providers/cloudfront/cloudfront.go
+++ providers/cloudfront/cloudfront.go
@@ -1,12 +1,54 @@
 // Package ips contains a list of current cloud flare IP ranges
 package cloudfront
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
 // CFIPs is the CloudFlare Server IP list (this is checked on build).
 func TrustedIPS() []string {
-	return []string{
-		"0.0.0.0/0",
-		"::/0",
+
+	url := "https://d7uri8nf7uskq.cloudfront.net/tools/list-cloudfront-ips"
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error making the request:", err)
+		return
 	}
+	defer resp.Body.Close() // Ensure the response body is closed
+
+	// Read the response body
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading the response body:", err)
+		return
+	}
+	// Define a map to hold the JSON data
+	var data map[string][]string
+
+	// Parse the JSON response
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		fmt.Println("Error parsing the JSON:", err)
+		return
+	}
+
+	// Extract the arrays
+	globalIPList, globalExists := data["CLOUDFRONT_GLOBAL_IP_LIST"]
+	regionalIPList, regionalExists := data["CLOUDFRONT_REGIONAL_EDGE_IP_LIST"]
+
+	if !globalExists && !regionalExists {
+		fmt.Println("Both keys are missing in the response")
+		return
+	}
+
+	// Merge the arrays
+	mergedIPList := append(globalIPList, regionalIPList...)
+	
+	return mergedIPList, nil
 }
 
 const ClientIPHeaderName = "Cloudfront-Viewer-Address"
